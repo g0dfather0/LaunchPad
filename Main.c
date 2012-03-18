63300bcd218624a697b6363d9c5b7e5c4444dbf6
g0dfather0 <g0dfather50@gmail.com>
2012-03-18 13:24:32 -0700

    housekeeping

--- Main.c
+++ Main.c
@@ -3,6 +3,14 @@
  * Author: g0dfather0
  * Project to explore the programming of the Launchpad
  * CPU Version: MSP430G2553
+ * 
+ * License:
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * g0dfather0 from GitHub wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
  */
  
 //Include header file for device
