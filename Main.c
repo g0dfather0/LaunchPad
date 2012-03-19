61dcce6ff2cad65ea696db53ff49adc07fd60e7d
g0dfather0 <g0dfather50@gmail.com>
2012-03-19 14:26:25 -0700

    housekeeping

--- Main.c
+++ Main.c
@@ -13,8 +13,8 @@
  * ----------------------------------------------------------------------------
  */
  
-#define MSP430g2452 1
-//#define MSP430g2553 1
+//#define MSP430g2452 1
+#define MSP430g2553 1
 
 //Include header file for device
 //LaunchPad came with these two devices to sample
@@ -33,7 +33,7 @@
 #define LED_OUT P1OUT //Assign variable for controlling LEDs (P1 output register)
  
 //Setting values for timer values for toggling LEDs
-#define LED_TOGGLE_RATE 0x3FFF //TODO: Need to find exact toggle rate frequency
+#define LED_TOGGLE_RATE 0x1F40 //
  
 //Declare Functions explicitly
 void InitializeLEDs(void);
@@ -43,7 +43,7 @@ void InitializeClocks(void);
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD; // Disable WatchDog Timer
-	__enable_interrupt(); //Enable Interrupts
+	//__enable_interrupt(); //Enable Interrupts
 	InitializeLEDs();
 	InitializeLEDEffect();
 }
@@ -57,9 +57,17 @@ void InitializeLEDs(void)
 void InitializeLEDEffect(void)
 {	
 	LED_OUT &= ~LED_RED; //Set Green LED on and Red LED off
+	
+	//Lower clock speed if needed. This app uses full Timer A clock speed so this setting is not needed
+	//BCSCTL1 |= DIVA_0; //Basic clock system control 1 |= Divide Clock by 1 (Full clock speed, 32kHz) 
+	//Same functionality uncommented, divide by 1 does nothing
+	
+	//Setting the clock source is necessary for running this app without the debugger natively on the CPU       
+  	BCSCTL3 |= LFXT1S_2; //Basic clock system control 3 ACLK = VLO (Very Low power/frequency Oscillator => 32kHz)
+  	
 	TACCR0 = LED_TOGGLE_RATE; //Set timer value
 	TACTL = TASSEL_1 + MC_1; //Timer A control = Clock Source Select (Timer A) + Timer A mode Control count up to Capture Control 0 (LED_TOGGLE_RATE)
-	TACCTL1 = CCIE + OUTMOD_3; //Timer A Capture Compare Control set to enable interrupt + Pulse Width Modulation Set/Reset 
+	TACCTL1 = CCIE + OUTMOD_0; //Timer A Capture Compare Control set to enable interrupt + Output Only (PWM) 
 	__bis_SR_register(LPM3_bits + GIE); //enable low power with interrupts turned on
 }
 
